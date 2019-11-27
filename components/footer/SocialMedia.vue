7023bd83819c15ba192d2f100642da00636b702b
alexandr <Venikeee1@gmail.com>
2019-11-28 01:30:31 +0200

    - created project and added header and footer

---
+++ components/footer/SocialMedia.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="social-media">
+      <li class="social-media__item">
+        <a href="" class="social-media__link">Social</a>
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'SocialMedia'
+}
+</script>
+
+<style scoped>
+
+</style>
